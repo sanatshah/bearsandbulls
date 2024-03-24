a2f09b82e36353c2102e98ce96da34990eb0ce6b
sunny <sunnyshah711@gmail.com>
2024-03-24 09:59:09 -0400

    full game working

--- api/screens/Losing.tsx
+++ api/screens/Losing.tsx
@@ -13,7 +13,7 @@ export const Losing = () => {
           backgroundColor: "#FAE9CB"
         }}
       >
-        <img src="https://i.imgur.com/CElU8pv.png" width="1200px" height="800px" style={{ objectFit: 'contain'}} />
+        <img src="https://i.imgur.com/CElU8pv.png" width="1200px" height="800px" style={{ objectFit: 'fill'}} />
       </div>
     ),
     intents: [
